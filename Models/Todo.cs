8ed7fb2b94ec26c3059646111b586d7d247815a9
Siqueiraaf <rafael.siqueira098@gmail.com>
2024-12-17 17:18:00 -0300

    Criando os Models Todo ErrorResponse DbSettings

---
+++ Models/Todo.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPi.Models
+{
+    public class Todo
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Priority { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Todo()
+        {
+            IsCompleted = false;
+        }
+    }
+}
